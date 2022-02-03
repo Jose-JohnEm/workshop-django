c3bd25333bb85e4d7293251e50875fec02430eef
NaadiQmmr <pirouettecacahuete@protonmail.com>
2022-02-03 14:10:20 +0100

    Final shape for WS

--- socialnetwork/posts/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from. import views
-
-urlpatterns = [
-    path('', views.FeedView.as_view(), name="/"),
-    path('upload_post/', views.create_post, name="upload_post"),
-    path('posts/<int:pk>/', views.create_comment, name="create-comment-view")
-]
--- socialnetwork/users/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import User
-from django.contrib.auth.admin import UserAdmin
-
-admin.site.register(User, UserAdmin)
-# Register your models here.
